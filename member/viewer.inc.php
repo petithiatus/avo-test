b72396c4afafd84253510bbd4059b5d0dd078d44
petithiatus <petit.hiatus@gmail.com>
2021-07-02 13:04:02 +0900

    edit member profile img

--- member/viewer.inc.php
+++ member/viewer.inc.php
@@ -1,9 +1,9 @@
 <?php
-if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가 
+if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 add_stylesheet('<link rel="stylesheet" href="'.G5_CSS_URL.'/style.profile.css">', 0);
 
 /** 스탯 이용 시 스탯 설정값 가져오기 **/
-if($article['ad_use_status']) { 
+if($article['ad_use_status']) {
 	$status = array();
 	$st_result = sql_query("select * from {$g5['status_config_table']} order by st_order asc");
 	for($i = 0; $row = sql_fetch_array($st_result); $i++) {
@@ -31,7 +31,7 @@ for($i = 0; $row = sql_fetch_array($ar_result); $i++) {
 	$ch['고유코드'] 로 해당 데이터를 가져올 수 있습니다.
 
 	--
-	
+
 	스탯 설정에서 추가한 캐릭터의 데이터를 임의로 뿌리고 싶을 때
 	$변수 = get_status_by_name($ch['ch_id'], 스탯명);
 	* $변수['has']	: 현재 캐릭터가 가지고 있는 전체값 (ex. 캐릭터의 최대 HP 값)
@@ -40,10 +40,10 @@ for($i = 0; $row = sql_fetch_array($ar_result); $i++) {
 	* $변수['max']	: 입력할 수 있는 최대값
 	* $변수['min']	: 필수 최소값
 	--
-	
+
 	자동으로 출력 되는게 아닌 임의로 레이아웃을 수정하고 싶을 땐
 	위쪽의 설정값 가져 오는 부분을 지우셔도 무방합니다.
-	
+
 	--
 
 
@@ -57,7 +57,7 @@ for($i = 0; $row = sql_fetch_array($av_result); $i++) {
 
 // ------- 캐릭터 의상 정보 가져오기
 $temp_cl = sql_fetch("select * from {$g5['closthes_table']} where ch_id = '{$ch_id}' and cl_use = '1'");
-if($temp_cl['cl_path']) { 
+if($temp_cl['cl_path']) {
 	$ch['ch_body'] = $temp_cl['cl_path'];
 }
 ?>
@@ -83,18 +83,18 @@ if($temp_cl['cl_path']) {
 	<? if($article['ad_use_body'] && $ch['ch_body']) { ?>
 	<div class="visual-area">
 		<div id="character_body">
-			<img src="<?=$ch['ch_body']?>" alt="캐릭터 전신" />
+			<img src="<?=$ch['ch_body']?>" alt="이미지2" />
 		</div>
 	<? } ?>
 
 	<? if($article['ad_use_head'] && $ch['ch_head']) { ?>
 		<div id="character_head" class="open">
 			<div class="contents">
-				<img src="<?=$ch['ch_head']?>" alt="캐릭터 흉상" />
+				<img src="<?=$ch['ch_head']?>" alt="이미지2" />
 			</div>
 
 			<a href="#character_head" class="ui-btn point full small toggle-head-pannel">
-				흉상 <span class="on">접기</span><span class="off">펼치기</span>
+				이미지2 <span class="on">접기</span><span class="off">펼치기</span>
 			</a>
 			<script>
 				$('.toggle-head-pannel').on('click', function() {
@@ -149,7 +149,7 @@ if($temp_cl['cl_path']) {
 				</td>
 			</tr>
 		<? } ?>
-		<? if($article['ad_use_rank']) { 
+		<? if($article['ad_use_rank']) {
 			// 랭킹정보 출력
 		?>
 			<tr>
@@ -159,7 +159,7 @@ if($temp_cl['cl_path']) {
 				</td>
 			</tr>
 		<? } ?>
-		<? if($article['ad_use_exp']) { 
+		<? if($article['ad_use_exp']) {
 			// 경험치 정보 출력
 		?>
 			<tr>
@@ -170,7 +170,7 @@ if($temp_cl['cl_path']) {
 				</td>
 			</tr>
 		<? } ?>
-		<? for($i=0; $i < count($ch_ar); $i++) { 
+		<? for($i=0; $i < count($ch_ar); $i++) {
 			// 추가 프로필 항목 출력
 			$ar = $ch_ar[$i];
 			$key = $ar['ar_code'];
@@ -180,7 +180,7 @@ if($temp_cl['cl_path']) {
 					<?=$ar['ar_name']?>
 				</th>
 				<?
-					if($ar['ar_type'] == 'file' || $ar['ar_type'] == 'url') { 
+					if($ar['ar_type'] == 'file' || $ar['ar_type'] == 'url') {
 						// 이미지 타입의 파일
 				?>
 
@@ -191,9 +191,9 @@ if($temp_cl['cl_path']) {
 				<? } else { ?>
 					<td>
 					<?
-						if($ar['ar_type'] == 'textarea') 
+						if($ar['ar_type'] == 'textarea')
 							echo nl2br($ch[$key]);
-						else 
+						else
 							echo $ch[$key];
 
 						if($ar['ar_type'] != 'textarea' && $ar['ar_type'] != 'select')
@@ -209,7 +209,7 @@ if($temp_cl['cl_path']) {
 <!-- // 캐릭터 기본정보 출력 영역 -->
 
 
-<? if($article['ad_use_closet'] && $article['ad_use_body'] && $mb['mb_id'] == $member['mb_id']) { 
+<? if($article['ad_use_closet'] && $article['ad_use_body'] && $mb['mb_id'] == $member['mb_id']) {
 	// 옷장 설정
 	// 옷장 사용 및 캐릭터 소유주일시에 출력
 	// -- 옷장 출력형태 변경을 원할 시, mypage/character/closet.inc.php 파일을 수정해 주시길 바랍니다.
@@ -222,7 +222,7 @@ if($temp_cl['cl_path']) {
 <? } ?>
 
 
-<? if($article['ad_use_status']) { 
+<? if($article['ad_use_status']) {
 	// 스탯 설정
 	// -- 스탯 출력형태 변경을 원하실 시, mypage/character/status.inc.php 파일을 수정해 주시길 바랍니다.
 ?>
@@ -240,7 +240,7 @@ if($temp_cl['cl_path']) {
 	</div>
 <? } ?>
 
-<? if($article['ad_use_title']) { 
+<? if($article['ad_use_title']) {
 	// 타이틀 설정
 	// -- 타이틀 출력형태 변경을 원하실 시, mypage/character/title.inc.php 파일을 수정해 주시길 바랍니다.
 ?>
@@ -253,14 +253,14 @@ if($temp_cl['cl_path']) {
 	</div>
 <? } ?>
 
-<? if($article['ad_use_inven']) { 
+<? if($article['ad_use_inven']) {
 	// 인벤토리 출력
 	// -- 인벤토리 출력형태 변경을 원하실 시, mypage/inventory/list.inc.php 파일을 수정해 주시길 바랍니다.
 ?>
 	<hr class="padding" />
 	<h3>
 		INVENTORY
-	<? if($article['ad_use_money']) { 
+	<? if($article['ad_use_money']) {
 		// 소지금 사용시 현재 보유 중인 소지금 출력
 	?>
 		<span style="float:right;">
@@ -296,5 +296,3 @@ if($temp_cl['cl_path']) {
 	<hr class="padding" />
 
 </div>
-
-
